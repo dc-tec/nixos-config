13bcc158d6d43ef3c4cbcee5b6ece8985a1d43aa
Roel de Cort <roel@decort.tech>
2024-04-06 21:52:47 +0200

    update secrets.nix

--- secrets.nix
+++ secrets.nix
@@ -2,7 +2,7 @@ let
   legion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIJ3XBgAWMTPR/gR2v5zOmKZKUs8RYkKp+MK7tOzPn9G";
   hosts = [ legion ];
 
-  roelc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIBU9qmqJ/OBagFNN+xKNcFVcpA5tB+OKbT30yC0Y4aD";
+  roelc = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIBU9qmqJ/OBagFNN+xKNcFVcpA5tB+OKbT30yC0Y4aD" ];
   users = roelc;
 
 in 
