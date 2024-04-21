188f4e1cfee8fd39f415eb30fedebcdda2507abb
Roel de Cort <roel@decort.tech>
2024-04-21 23:34:43 +0200

    persist data

--- modules/graphical/applications/firefox/default.nix
+++ modules/graphical/applications/firefox/default.nix
@@ -6,6 +6,8 @@
   options.dc-tec.graphical.applications.firefox.enable = lib.mkEnableOption "firefox";
 
   config = lib.mkIf config.dc-tec.graphical.applications.firefox.enable {
+    dc-tec.core.zfs.homeCacheLinks = [".mozilla"];
+
     home-manager.users.roelc = {pkgs, ...}: {
       programs.firefox = {
         enable = true;
