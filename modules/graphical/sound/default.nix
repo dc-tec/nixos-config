33fbe52124eb1596986ca82f22c57a887b48ec98
Roel de Cort <roel@decort.tech>
2024-05-09 01:42:57 +0200

    update sound

--- modules/graphical/sound/default.nix
+++ modules/graphical/sound/default.nix
@@ -18,7 +18,8 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      jack.enable = true;
+      wireplumber.enable = true;
+      #jack.enable = true;
     };
   };
 }
