ad7c91a59417fe0899bf25861880197cfcd874b1
Roel de Cort <roel@decort.tech>
2024-05-17 23:51:22 +0200

    we only use tenv

--- modules/development/terraform/default.nix
+++ modules/development/terraform/default.nix
@@ -11,10 +11,6 @@
   };
 
   config = lib.mkIf config.dc-tec.development.terraform.enable {
-    nixpkgs.config.allowUnfreePredicate = pkg:
-      builtins.elem (lib.getName pkg) [
-        "terraform"
-      ];
     environment.systemPackages = with pkgs; [
       tenv
       terraform-docs
