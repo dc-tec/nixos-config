3aacf2554eb0fc5f7d3a9ae41a0e9daa14cd1939
Roel de Cort <roel@decort.tech>
2024-06-22 01:32:15 +0200

    hyprland changes based on end4 dots

--- modules/graphical/desktop/hyprland/default.nix
+++ modules/graphical/desktop/hyprland/default.nix
@@ -54,6 +54,7 @@
         };
       };
 
+      ## Took some stuff from the end4 dots config @ https://github.com/end-4/dots-hyprland/blob/main/.config/hypr/hyprland/general.conf
       wayland.windowManager.hyprland = {
         enable = true;
         catppuccin.enable = true;
@@ -81,7 +82,7 @@
             gaps_out = 6;
             border_size = 2;
             layout = "dwindle";
-            allow_tearing = false;
+            allow_tearing = true;
           };
 
           input = {
@@ -95,28 +96,63 @@
           };
 
           decoration = {
-            rounding = 2;
+            rounding = 15;
             active_opacity = 0.9;
             inactive_opacity = 0.8;
             fullscreen_opacity = 0.9;
+
             blur = {
               enabled = true;
-              size = 3;
-              passes = 1;
+              xray = true;
+              special = false;
+              new_optimizations = true;
+              size = 14;
+              passes = 4;
+              brightness = 1;
+              noise = 0.01;
+              contrast = 1;
+              popups = true;
+              popups_ignorealpha = 0.6;
               ignore_opacity = false;
             };
+
+            drop_shadow = true;
+            shadow_ignore_window = true;
+            shadow_range = 20;
+            shadow_offset = "0 2";
+            shadow_render_power = 4;
           };
 
           animations = {
             enabled = true;
-            bezier = "myBezier, 0.05, 0.9, 0.1, 1.0";
+            bezier = [
+              "linear, 0, 0, 1, 1"
+              "md3_standard, 0.2, 0, 0, 1"
+              "md3_decel, 0.05, 0.7, 0.1, 1"
+              "md3_accel, 0.3, 0, 0.8, 0.15"
+              "overshot, 0.05, 0.9, 0.1, 1.1"
+              "crazyshot, 0.1, 1.5, 0.76, 0.92"
+              "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
+              "menu_decel, 0.1, 1, 0, 1"
+              "menu_accel, 0.38, 0.04, 1, 0.07"
+              "easeInOutCirc, 0.85, 0, 0.15, 1"
+              "easeOutCirc, 0, 0.55, 0.45, 1"
+              "easeOutExpo, 0.16, 1, 0.3, 1"
+              "softAcDecel, 0.26, 0.26, 0.15, 1"
+              "md2, 0.4, 0, 0.2, 1"
+            ];
             animation = [
-              "windows, 1, 7, myBezier"
-              "windowsOut, 1, 7, default, popin 80%"
+              "windows, 1, 3, md3_decel, popin 60%"
+              "windowsIn, 1, 3, md3_decel, popin 60%"
+              "windowsOut, 1, 3, md3_accel, popin 60%"
               "border, 1, 10, default"
-              "borderangle, 1, 8, default"
-              "fade, 1, 7, default"
-              "workspaces, 1, 6, default"
+              "fade, 1, 3, md3_decel"
+              "layersIn, 1, 3, menu_decel, slide"
+              "layersOut, 1, 1.6, menu_accel"
+              "fadeLayersIn, 1, 2, menu_decel"
+              "fadeLayersOut, 1, 4.5, menu_accel"
+              "workspaces, 1, 7, menu_decel, slide"
+              "specialWorkspace, 1, 3, md3_decel, slidevert"
             ];
           };
 
@@ -124,6 +160,8 @@
             pseudotile = true;
             preserve_split = true;
             no_gaps_when_only = 0;
+            smart_split = false;
+            smart_resizing = false;
           };
 
           misc = {
@@ -185,7 +223,14 @@
             "$mod ALT, e, exec, $terminal --hold -e ${pkgs.yazi}/bin/yazi"
             "$mod ALT, o, exec, ${pkgs.obsidian}/bin/obsidian"
             "$mod, r, exec, pkill fuzzel || ${pkgs.fuzzel}/bin/fuzzel"
-            "$mod ALT, Slash, exec, pkill anyrun || ${pkgs.anyrun}/bin/anyrun"
+            "$mod ALT, r, exec, pkill anyrun || ${pkgs.anyrun}/bin/anyrun"
+
+            # Clipboard
+            "$mod ALT, v, exec, pkill fuzzel || cliphist list | fuzzel --no-fuzzy --dmenu | cliphist decode | wl-copy"
+
+            # Screencapture
+            "$mod, S, exec, ${pkgs.grim}/bin/grim | wl-copy"
+            "$mod SHIFT+ALT, S, exec, ${pkgs.grim}/bin/grim -g \"$(slurp)\" - | ${pkgs.swappy}/bin/swappy -f -"
           ];
 
           bindm = [
