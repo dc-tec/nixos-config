b9ad40fcc53e361db22b38eb8e0034b7aecdf258
Roel de Cort <roel@decort.tech>
2024-08-25 01:15:46 +0200

    use unstable for awscli

--- modules/development/aws-cli/default.nix
+++ modules/development/aws-cli/default.nix
@@ -12,7 +12,7 @@
 
   config = lib.mkIf config.dc-tec.development.aws-cli.enable {
     home-manager.users.roelc = {
-      home.packages = with pkgs.stable; [
+      home.packages = with pkgs; [
         awscli2
       ];
     };
