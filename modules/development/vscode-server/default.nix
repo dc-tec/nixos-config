e71b5d9dc95c43fb0e8fa960b3ca27dd3928baee
Roel de Cort <roel@decort.tech>
2024-10-28 23:27:13 +0100

    update vscode-server

--- modules/development/vscode-server/default.nix
+++ modules/development/vscode-server/default.nix
@@ -15,7 +15,7 @@
       imports = [
         "${fetchTarball {
           url = "https://github.com/msteen/nixos-vscode-server/tarball/master";
-          sha256 = "1rq8mrlmbzpcbv9ys0x88alw30ks70jlmvnfr2j8v830yy5wvw7h";
+          sha256 = "09j4kvsxw1d5dvnhbsgih0icbrxqv90nzf0b589rb5z6gnzwjnqf";
         }}/modules/vscode-server/home.nix"
       ];
 
