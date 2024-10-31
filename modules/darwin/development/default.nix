c8b148cc6b9a9fdaf81719e8d5d02f3a5827ddae
Roel de Cort <roel@decort.tech>
2024-11-01 00:05:40 +0100

    restructure

--- modules/darwin/development/default.nix
+++ modules/darwin/development/default.nix
@@ -4,22 +4,23 @@
   ];
 
   home-manager.users.roelc = {
-    home.packages = with pkgs; [
-      ansible-lint
-      awscli2
-      azure-cli
-      go
-      tenv
-      terraform-docs
-      powershell
-      yaml-language-server
-      talosctl
-      kubectl
-      kubernetes-helm
-      kustomize
-      argocd
-      cilium-cli
-      kubeseal
-    ];
+    home = {
+      packages = with pkgs; [
+        ansible-lint
+        awscli2
+        go
+        tenv
+        terraform-docs
+        powershell
+        yaml-language-server
+        talosctl
+        kubectl
+        kubernetes-helm
+        kustomize
+        argocd
+        cilium-cli
+        kubeseal
+      ];
+    };
   };
 }
