9505e277d5ce0428b5ad1fbd32fbbfe0c6d327d7
Roel de Cort <roel@decort.tech>
2026-07-29 01:26:03 +0200

    feat(darwin): add forge client tooling
    Signed-off-by: Roel de Cort <roel@decort.tech>

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -4,6 +4,11 @@
   ...
 }:
 {
+  programs.mas = {
+    enable = true;
+    packages.WireGuard = 1451685025;
+  };
+
   home-manager.users.${config.dc-tec.user.name}.home.packages = with pkgs; [
     radicle-node
     radicle-tui
